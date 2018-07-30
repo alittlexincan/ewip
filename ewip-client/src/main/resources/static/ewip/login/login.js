274e7f91c122e64cf7e0cc5583f1a4ec86da280b
alittlexincan <alittlexincan@sina.com>
2018-07-30 20:26:05 +0800

    git测试

--- ewip-client/src/main/resources/static/ewip/login/login.js
+++ ewip-client/src/main/resources/static/ewip/login/login.js
@@ -18,6 +18,8 @@ layui.use(['form'], function(){
 
     //监听提交
     form.on('submit(submit)', function(data){
+        console.log(data);
+        console.log("github");
         $.ajax({
             async:true,
             type: "POST",
