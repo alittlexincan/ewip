b59bab6cea5d37848849669ac3a2fbc492f3a00d
alittlexincan <alittlexincan@sina.com>
2018-08-01 12:00:19 +0800

    完善预警编辑页面，添加时间加载

--- ewip-client/src/main/resources/static/ewip/warn/edit.js
+++ ewip-client/src/main/resources/static/ewip/warn/edit.js
@@ -1,82 +1,129 @@
+layui.config({
+    base: '/client/layuiadmin/modules/'
+}).extend({
+    zTree: 'zTree',
+    selectTree: 'selectTree'
+});
 
-layui.use(['table','form','laydate','element','laytpl','layer'], function(){
+layui.use(['table','form','laydate','element','laytpl','layer','zTree','selectTree'], function(){
     let table = layui.table			// 引用layui表格
         ,form = layui.form			// 引用layui表单
         ,laytpl = layui.laytpl		// 引用layui模板引擎
         ,layer = layui.layer		// 引用layui弹出层
         ,$ = layui.$   			// 引用layui的jquery
         ,element = layui.element
-        ,laydate = layui.laydate;
-
-        //执行一个laydate实例
-        laydate.render({
-            elem: '#test1' //指定元素
-            ,type: 'datetime'
-        });
+        ,laydate = layui.laydate
+        ,zTree = layui.zTree
+        ,selectTree = layui.selectTree;
 
     /**
-     *
+     * 初始化发布时间
      */
-    $.ajax({
-        async:true
-        ,type: "POST"
-        ,data: {type: 0}
-        ,url: "/client/channel/list"
-        ,dataType: 'json'
-        ,success: function(json){
-            if(json.code == 200 && json.data != null){
-                var html ="";
-                json.data.forEach(function (currentValue, index, arr) {
-                    html += "<div class='imgbox' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
-                    html += "   <img src='/client/"+currentValue.icon+"' alt='"+currentValue.name+"' />";
-                    html += "<span>"+currentValue.name+"</span>";
-                    html += "</div>";
-                });
-                // $(".channel-list").empty().append(html);
+    laydate.render({
+        elem: '#editTime'
+        ,type: 'datetime'
+    });
+    laydate.render({
+        elem: '#forecastTime'
+        ,type: 'datetime'
+    });
+    laydate.render({
+        elem: '#invalidTime'
+        ,type: 'datetime'
+    });
+
+    /**
+     * 初始化加载渠道
+     */
+    var initChannel = function(){
+        $.ajax({
+            async:true
+            ,type: "POST"
+            ,data: {type: 0}
+            ,url: "/client/channel/list"
+            ,dataType: 'json'
+            ,success: function(json){
+                if(json.code == 200 && json.data != null){
+                    var html ="";
+                    json.data.forEach(function (currentValue, index, arr) {
+                        var st = "";
+                        if(currentValue.name == '短信' || currentValue.name == '大喇叭' || currentValue.name == '北斗'){
+                            st = "active";
+                        }else{
+                            st = "";
+                        }
+                        html += "<div class='imgbox "+ st +"' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
+                        html += "   <img src='/client/"+currentValue.icon+"' alt='"+currentValue.name+"' />";
+                        html += "<span>"+currentValue.name+"</span>";
+                        html += "</div>";
+                    });
+                    $(".channel-list").empty().append(html);
+                }
+            }
+        });
+    };
+
+    /**
+     * 初始化加载群组树
+     */
+    zTree.async({
+        id: "#areaTree",
+        setting: {
+            async:{
+                enable:true,
+                url: "/client/tree/area",
+                autoParam:["id"],
+                dataType:"json",
+            },
+            check: {
+                enable: false,
+                chkboxType: {"Y":"", "N": ""},
+                chkStyle:"checkbox"
+            },
+            data: {
+                simpleData: {
+                    enable: true
+                }
+            },
+            callback:{
+                onClick:null
             }
         }
     });
 
-
-    //触发事件
-    var active = {
-        tabAdd: function(obj){
-            console.log(obj)
-            //新增一个Tab项
-            element.tabAdd('ew-release-channel', {
-                title: '<li><div class="ccc"><img src="../../asseat/images/'+obj.channel+'.png" alt="' + obj.title + '" title="' +obj.title+'"></div></li>'
-                ,content: obj.channel
-                ,id: obj.id //实际使用一般是规定好的id
-            })
-        }
-        ,tabDelete: function(othis){
-            //删除指定Tab项
-            element.tabDelete('ew-release-channel', '44'); //删除：“商品管理”
-            othis.addClass('layui-btn-disabled');
-        }
-        ,tabChange: function(){
-            //切换到指定Tab项
-            element.tabChange('ew-release-channel', '22'); //切换到：用户管理
-        }
+    /**
+     * 初始化加载渠道
+     */
+    var initChannel = function(){
+        $.ajax({
+            async:true
+            ,type: "POST"
+            ,data: {type: 0}
+            ,url: "/client/channel/list"
+            ,dataType: 'json'
+            ,success: function(json){
+                if(json.code == 200 && json.data != null){
+                    var html ="";
+                    json.data.forEach(function (currentValue, index, arr) {
+                        var st = "";
+                        if(currentValue.name == '短信' || currentValue.name == '大喇叭' || currentValue.name == '北斗'){
+                            st = "active";
+                        }else{
+                            st = "";
+                        }
+                        html += "<div class='imgbox "+ st +"' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
+                        html += "   <img src='/client/"+currentValue.icon+"' alt='"+currentValue.name+"' />";
+                        html += "<span>"+currentValue.name+"</span>";
+                        html += "</div>";
+                    });
+                    $(".channel-list").empty().append(html);
+                }
+            }
+        });
     };
 
-    $('.imgbox').on('click', function(){
-        var othis = $(this),
-            type = othis.data('type'),
-            id = othis.data('id'),
-            channel = othis.data('channel')
-            title = othis.data('title')
-
-            obj = {
-                othis: othis,
-                id: id,
-                channel:channel,
-                title: title
-            },
-
-            active[type] ? active[type].call(this, obj) : '';
-
-    });
-
-
+    /**
+     * 初始化加载项
+     */
+    initChannel();  // 初始化加载渠道
 });
