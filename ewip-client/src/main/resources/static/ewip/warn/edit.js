c7120fe2d4fa256bc8090fcd5920d66db3ec5d7d
alittlexincan <alittlexincan@sina.com>
2018-08-01 14:34:29 +0800

    预警编辑界面添加渠道选中，日期交互

--- ewip-client/src/main/resources/static/ewip/warn/edit.js
+++ ewip-client/src/main/resources/static/ewip/warn/edit.js
@@ -22,79 +22,63 @@ layui.use(['table','form','laydate','element','laytpl','layer','zTree','selectTr
     laydate.render({
         elem: '#editTime'
         ,type: 'datetime'
+        ,theme: 'molv'
+        ,value: new Date()
+        ,format: 'yyyy-MM-dd HH:mm:ss'
     });
+    /**
+     * 初始化预计发生时间
+     */
     laydate.render({
         elem: '#forecastTime'
         ,type: 'datetime'
+        ,theme: 'molv'
+        ,format: 'yyyy-MM-dd HH:mm:ss'
     });
+    /**
+     * 初始化失效时间
+     */
     laydate.render({
         elem: '#invalidTime'
         ,type: 'datetime'
+        ,theme: 'molv'
+        ,format: 'yyyy-MM-dd HH:mm:ss'
     });
-
-    /**
-     * 初始化加载渠道
-     */
-    var initChannel = function(){
-        $.ajax({
-            async:true
-            ,type: "POST"
-            ,data: {type: 0}
-            ,url: "/client/channel/list"
-            ,dataType: 'json'
-            ,success: function(json){
-                if(json.code == 200 && json.data != null){
-                    var html ="";
-                    json.data.forEach(function (currentValue, index, arr) {
-                        var st = "";
-                        if(currentValue.name == '短信' || currentValue.name == '大喇叭' || currentValue.name == '北斗'){
-                            st = "active";
-                        }else{
-                            st = "";
-                        }
-                        html += "<div class='imgbox "+ st +"' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
-                        html += "   <img src='/client/"+currentValue.icon+"' alt='"+currentValue.name+"' />";
-                        html += "<span>"+currentValue.name+"</span>";
-                        html += "</div>";
-                    });
-                    $(".channel-list").empty().append(html);
-                }
-            }
-        });
-    };
 
     /**
      * 初始化加载群组树
      */
-    zTree.async({
-        id: "#areaTree",
-        setting: {
-            async:{
-                enable:true,
-                url: "/client/tree/area",
-                autoParam:["id"],
-                dataType:"json",
-            },
-            check: {
-                enable: false,
-                chkboxType: {"Y":"", "N": ""},
-                chkStyle:"checkbox"
-            },
-            data: {
-                simpleData: {
-                    enable: true
+    var initAreaTree = function () {
+        zTree.async({
+            id: "#areaTree",
+            setting: {
+                async:{
+                    enable:true,
+                    url: "/client/tree/area",
+                    autoParam:["id"],
+                    dataType:"json",
+                },
+                check: {
+                    enable: true,
+                    chkboxType: {"Y":"", "N": ""},
+                    chkStyle:"checkbox"
+                },
+                data: {
+                    simpleData: {
+                        enable: true
+                    }
+                },
+                callback:{
+                    onClick:null
                 }
-            },
-            callback:{
-                onClick:null
             }
-        }
-    });
+        });
+    };
 
     /**
      * 初始化加载渠道
      */
-    var initChannel = function(){
+    var initChannelList = function(){
         $.ajax({
             async:true
             ,type: "POST"
@@ -105,13 +89,7 @@ layui.use(['table','form','laydate','element','laytpl','layer','zTree','selectTr
                 if(json.code == 200 && json.data != null){
                     var html ="";
                     json.data.forEach(function (currentValue, index, arr) {
-                        var st = "";
-                        if(currentValue.name == '短信' || currentValue.name == '大喇叭' || currentValue.name == '北斗'){
-                            st = "active";
-                        }else{
-                            st = "";
-                        }
-                        html += "<div class='imgbox "+ st +"' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
+                        html += "<div class='imgbox' data-id='"+currentValue.id+"' data-title='"+currentValue.name+"' data-channel='"+currentValue.name+"' data-type='tabAdd'>";
                         html += "   <img src='/client/"+currentValue.icon+"' alt='"+currentValue.name+"' />";
                         html += "<span>"+currentValue.name+"</span>";
                         html += "</div>";
@@ -123,7 +101,32 @@ layui.use(['table','form','laydate','element','laytpl','layer','zTree','selectTr
     };
 
     /**
+     * 渠道全选、反选
+     */
+    $(".channel-option").on("click", "div > span", function(element) {
+        var text = $(this).text(),
+            event = $(this).data("event");
+
+        if(text == '全选'){
+            $("." + event + " .imgbox").addClass("active");
+        }else{
+            $("." + event + " .imgbox").removeClass("active");
+        }
+    });
+    /**
+     * 渠道点击单选、取消选择
+     */
+    $(".channel-list").on("click", ".imgbox", function(element) {
+        if($(this).hasClass("active")){
+            $(this).removeClass("active");
+        }else{
+            $(this).addClass("active");
+        }
+    });
+
+    /**
      * 初始化加载项
      */
-    initChannel();  // 初始化加载渠道
+    initChannelList();  // 初始化加载渠道
+    initAreaTree();     // 初始化加载地区
 });
