719d949c45b88cbb4c39b9e3e2b3f6bf7908fb80
alittlexincan <alittlexincan@sina.com>
2018-11-07 13:42:05 +0800

    增加规范制度

---
+++ ewip-client/src/main/resources/static/ewip/standard/standard.js
@@ -0,0 +1,14 @@
+
+layui.use(["table","form","laytpl","layer"], function(){
+    let table = layui.table			// 引用layui表格
+        ,form = layui.form			// 引用layui表单
+        ,laytpl = layui.laytpl		// 引用layui模板引擎
+        ,layer = layui.layer		// 引用layui弹出层
+        ,$ = layui.$;   			// 引用layui的jquery
+
+
+
+
+    window.location.href="/client/standard/download?url="+ "/base-standard-file/附件1：基层气象灾害预警服务规范.doc";
+});
+
