329702b23bd68f02077a9b73c4bb2140ccf7f258
alittlexincan <alittlexincan@sina.com>
2019-01-14 17:08:42 +0800

    修改message

--- message-publisher/src/main/java/com/zhxu/message/modal/Message.java
+++ message-publisher/src/main/java/com/zhxu/message/modal/Message.java
@@ -2,10 +2,13 @@ package com.zhxu.message.modal;
 
 import lombok.Data;
 
+import java.io.File;
+
 @Data
 public class Message {
     private Area area;
     private String content;
+    private File[] files;
     private Channel channel;
     private Group[] groups;
 }
